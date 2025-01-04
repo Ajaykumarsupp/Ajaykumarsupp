8a23b9d5ffddb24cf424f065751088c2c6bf5ba7
Ajaykumarsupp <ajaykumarsuppa26@gmail.com>
2025-01-04 11:28:52 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hel\"lo\"World!");
+        System.out.println(3 + 8);
+        System.out.println(3 - 8);
+        System.out.println(3 * 8);
+        System.out.println(3 % 8);
+        System.out.println(3 / 8);
+
+        System.out.println(Integer.SIZE);
+        System.out.println(Byte.SIZE);
+
+        int num = 84;
+        System.out.println(num);
+
+        int num1 = Integer.MAX_VALUE;
+        int num2 = Integer.MIN_VALUE;
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
